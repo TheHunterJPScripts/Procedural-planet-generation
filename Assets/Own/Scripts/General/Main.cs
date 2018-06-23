2af1f1b3cd2a131a9c23495c493d5e4ff2d564fd
TheHunterJPScripts <32091347+TheHunterJPScripts@users.noreply.github.com>
2018-06-23 22:26:31 +0200

    Add files via upload

---
+++ Assets/Own/Scripts/General/Main.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Generation;
+
+public class Main : MonoBehaviour
+{
+    public RuntimeAnimatorController controller;
+    public string name;
+    public Vector3 position;
+    public int seed;
+    public GenerationData[] general;
+    public Material ma;
+    public int radius;
+    public int sphereSubdivision;
+    public int chunckSubdivision;
+
+    public void Start()
+    {
+        seed = Random.Range(0, int.MaxValue);
+        Planet.AddPlanetToQueue(name, position, seed, general, ma, radius, sphereSubdivision, chunckSubdivision, controller);
+        Planet.StartDataQueue();
+    }
+    private void Update()
+    {
+        Planet.StartGeneratingMeshDataQueue();
+        Planet.InstantiateIntoWorld();
+    }
+}
